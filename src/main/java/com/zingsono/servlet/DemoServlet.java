40c887d34f055c46a7c89350bc1eced448db9924
pine <916931772@qq.com>
2017-07-29 02:43:50 +0800

    demo

---
+++ src/main/java/com/zingsono/servlet/DemoServlet.java
@@ -0,0 +1,18 @@
+package com.zingsono.servlet;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.Date;
+
+@WebServlet("/demoServlet")
+public class DemoServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.getWriter().print("Demo Servlet Print "+ new Date());
+    }
+}
